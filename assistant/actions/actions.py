b78d104df690241b3e5919e58c99f913d18b44e8
JustinaPetr <juste@rasa.com>
2021-02-08 18:31:45 +0000

    create a new branch

--- assistant/actions/actions.py
+++ assistant/actions/actions.py
@@ -2,7 +2,6 @@ import logging
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import AllSlotsReset, SlotSet, EventType, SessionStarted, ActionExecuted
-from rasa_sdk.forms import FormAction, FormValidationAction, REQUESTED_SLOT
 import pandas as pd
 
 class IncidentStatus(Action):
@@ -24,29 +23,3 @@ class IncidentStatus(Action):
 
         return [SlotSet("email", email)]
 
-
-class OpenIncident(Action):
-
-    def name(self):
-        return 'action_open_incident'
-
-    def run(self, dispatcher, tracker, domain):
-
-        incident_title = tracker.get_slot('incident_title')
-        dispatcher.utter_message("The ticket for your problem {} has been opened.".format(incident_title))
-
-        return []
-
-
-class ValidateOpenIncidentForm(FormValidationAction):
-
-    def name(self):
-        return 'validate_open_incident_form'
-
-    def validate_problem_description(self, slot_value, dispatcher, tracker, domain):
-
-        if len(slot_value.split())>1:
-            return {"problem_description": slot_value}
-        else:
-            dispatcher.utter_message("The description is too short. Please rephrase")
-            return {"problem_description": None}
